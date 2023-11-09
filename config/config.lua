1178191c4a848d972cd9fe4476186673979e242e
Cian Murray <cianseamusmurray@gmail.com>
2023-11-09 18:55:16 +0000

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,7 +1,14 @@
 Config = {}
 
 Config.departments = {
-    ["police"] = "police.duty",
-    ["ems"] = "ems.duty",
-    -- Add more departments by copy and pasting
+    ["SAST"] = "sast.duty", -- I would suggest it being a abbreivation like SAST or FBI
+    ["FBI"] = "fbi.duty",
 }
+
+-- The webhooks must only be created for departments above, and there can only be one webhook per department
+Config.webhooks = {
+    ["police"] = "https://discord.com/api/webhooks/your-webhook-url",
+    ["ems"] = "https://discord.com/api/webhooks/your-webhook-url",
+}
+
+return Config
