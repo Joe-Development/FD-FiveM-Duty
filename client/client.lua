f9ab950d90ec8097dcd9b95eb3e0a3b9eb08291c
Cian Murray <cianseamusmurray@gmail.com>
2023-11-09 19:26:43 +0000

    Added exports and changed commands a we bit

--- client/client.lua
+++ client/client.lua
@@ -1,11 +1,22 @@
+local OnDuty = false
+
 RegisterCommand("duty", function(source, args)
-    local department = args[1]
-    local callsign = args[2]
+    local department = tostring(args[1])
+    local callsign = table.concat(args[2], "")
 
     TriggerServerEvent("fd-duty:onDuty", department, callsign)
-end)
+end, false)
 
 RegisterCommand("offduty", function(source, args)
-    local department = args[1]
+    local department = tostring(args[1])
     TriggerServerEvent("fd-duty:offDuty", department)
+end, false)
+
+RegisterNetEvent("fd-duty:SetOnDuty")
+AddEventHandler("fd-duty:SetOnDuty", function(isOnDuty)
+    OnDuty = isOnDuty
+end)
+
+exports("IsPlayerOnDuty", function()
+    return OnDuty
 end)
--- server/server.lua
+++ server/server.lua
@@ -4,34 +4,32 @@ AddEventHandler("fd-duty:onDuty", function(department)
     local discord = ExtractIdentifiers(source).discord:gsub("discord:", "")
     local currentTime = os.time() / 60
 
-    local result = MySQL.Sync.fetchScalar("SELECT start_time FROM onduty WHERE discord = @discord AND department = @department ORDER BY start_time DESC LIMIT 1", {
-        ['@discord'] = discord,
-        ['@department'] = department
-    })
+    if OnDuty[source] then
+        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You are already on duty as a " .. OnDuty[source])
+        return
+    end
 
-    if result then
-        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You are already on duty as a " .. department)
-    else
-        if Config.departments[department] then
-            if IsPlayerAceAllowed(source, Config.departments[department]) then
-                local insertResult = MySQL.Sync.insert("INSERT INTO onduty (discord, department, callsign, start_time) VALUES (@discord, @department, @callsign, @startTime)", {
-                    ['@discord'] = discord,
-                    ['@department'] = department,
-                    ['@callsign'] = "", 
-                    ['@startTime'] = currentTime
-                })
+    if Config.departments[department] then
+        if IsPlayerAceAllowed(source, Config.departments[department]) then
+            local insertResult = MySQL.Sync.insert("INSERT INTO onduty (discord, department, callsign, start_time) VALUES (@discord, @department, @callsign, @startTime)", {
+                ['@discord'] = discord,
+                ['@department'] = department,
+                ['@callsign'] = "", 
+                ['@startTime'] = currentTime
+            })
 
-                if insertResult then
-                    TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 255, 255}, "You are now on duty as a " .. department)
-                else
-                    TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Failed to go on duty. Please try again.")
-                end
+            if insertResult then
+                OnDuty[source] = department
+                TriggerClientEvent("fd-duty:SetOnDuty", source, true)
+                TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 255, 255}, "You are now on duty as a " .. department)
             else
-                TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You don't have permission to go on duty as a " .. department)
+                TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Failed to go on duty. Please try again.")
             end
         else
-            TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Invalid department")
+            TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You don't have permission to go on duty as a " .. department)
         end
+    else
+        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Invalid department")
     end
 end)
 
@@ -39,47 +37,37 @@ RegisterServerEvent("fd-duty:offDuty")
 AddEventHandler("fd-duty:offDuty", function(department)
     local source = source
     local discord = ExtractIdentifiers(source).discord:gsub("discord:", "")
-    local currentTime = os.time() / 60 
+    local currentTime = os.time() / 60 -- Convert to minutes
 
-    local result = MySQL.Sync.fetchScalar("SELECT start_time FROM onduty WHERE discord = @discord AND department = @department ORDER BY start_time DESC LIMIT 1", {
+    if not OnDuty[source] then
+        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You are not currently on duty")
+        return
+    end
+
+    if OnDuty[source] ~= department then
+        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You are on duty as a " .. OnDuty[source] .. ". Use /ofduty " .. OnDuty[source] .. " to go off duty")
+        return
+    end
+
+    local insertResult = MySQL.Sync.insert("INSERT INTO dutylogs (discord, department, callsign, time) VALUES (@discord, @department, @callsign, @dutyTime)", {
         ['@discord'] = discord,
-        ['@department'] = department
+        ['@department'] = department,
+        ['@callsign'] = "",
+        ['@dutyTime'] = currentTime
     })
 
-    if result then
-        local startTime = tonumber(result)
-        local dutyTime = currentTime - startTime
-
-        local insertResult = MySQL.Sync.insert("INSERT INTO dutylogs (discord, department, callsign, time) VALUES (@discord, @department, @callsign, @dutyTime)", {
-            ['@discord'] = discord,
-            ['@department'] = department,
-            ['@callsign'] = "", 
-            ['@dutyTime'] = dutyTime
-        })
-
-        if insertResult then
-            local webhookUrl = Config.webhooks[department] -- Replace with your webhook URL
-            local message = {
-                ["username"] = "Duty Log",
-                ["content"] = "<@" .. discord .. "> has gone off duty.",
-                ["embeds"] = {{
-                    ["title"] = "Duty Log",
-                    ["description"] = "Department: " .. department .. "\nTime: " .. dutyTime .. " minutes",
-                    ["color"] = 16711680 -- Red color
-                }}
-            }
-
-            PerformHttpRequest(webhookUrl, function(statusCode, response, headers) end, "POST", json.encode(message), {["Content-Type"] = "application/json"})
-
-            TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 255, 255}, "You are now off duty")
-        else
-            TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Failed to log duty. Please try again.")
-        end
+    if insertResult then
+        OnDuty[source] = nil
+        TriggerClientEvent("fd-duty:SetOnDuty", source, false)
+        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 255, 255}, "You are now off duty")
     else
-        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "No on-duty record found for your department")
+        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Failed to log duty. Please try again.")
     end
 end)
 
+exports("GetOnDutyPlayers", function()
+    return OnDuty
+end)
 
 function ExtractIdentifiers(src)
     local identifiers = {
