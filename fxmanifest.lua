112c1278039d5df78010cd9babb4fe43e6e048fa
Cian Murray <cianseamusmurray@gmail.com>
2023-11-09 17:57:22 +0000

    Created base

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game {'gta5'}
+
+author 'freech_dev'
+
+description 'A simple open source FiveM duty script'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
