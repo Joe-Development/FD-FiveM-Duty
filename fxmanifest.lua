707847c89884f07f07e10f4706d3fe080b615dcf
Cian Murray <cianseamusmurray@gmail.com>
2023-11-09 19:03:28 +0000

    Updated fxmanifest.lua and server.lua
    Added config to the fxmanifest, and updated the server.lua with checks if the user is already onduty or not

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,8 @@ author 'freech_dev'
 
 description 'A simple open source FiveM duty script'
 
+shared_script 'config/config.lua'
+
 client_scripts {
     'client/client.lua'
 }
--- server/server.lua
+++ server/server.lua
@@ -1,28 +1,37 @@
 RegisterServerEvent("fd-duty:onDuty")
-AddEventHandler("fd-duty:onDuty", function(department, callsign)
+AddEventHandler("fd-duty:onDuty", function(department)
     local source = source
     local discord = ExtractIdentifiers(source).discord:gsub("discord:", "")
-    local startTime = os.time() / 60 
+    local currentTime = os.time() / 60
 
-    if Config.departments[department] then
-        if IsPlayerAceAllowed(source, Config.departments[department]) then
-            local insertResult = MySQL.Sync.insert("INSERT INTO onduty (discord, department, callsign, start_time) VALUES (@discord, @department, @callsign, @startTime)", {
-                ['@discord'] = discord,
-                ['@department'] = department,
-                ['@callsign'] = callsign,
-                ['@startTime'] = startTime
-            })
+    local result = MySQL.Sync.fetchScalar("SELECT start_time FROM onduty WHERE discord = @discord AND department = @department ORDER BY start_time DESC LIMIT 1", {
+        ['@discord'] = discord,
+        ['@department'] = department
+    })
 
-            if insertResult then
-                TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 255, 255}, "You are now on duty as a " .. department .. " with callsign " .. callsign)
+    if result then
+        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You are already on duty as a " .. department)
+    else
+        if Config.departments[department] then
+            if IsPlayerAceAllowed(source, Config.departments[department]) then
+                local insertResult = MySQL.Sync.insert("INSERT INTO onduty (discord, department, callsign, start_time) VALUES (@discord, @department, @callsign, @startTime)", {
+                    ['@discord'] = discord,
+                    ['@department'] = department,
+                    ['@callsign'] = "", 
+                    ['@startTime'] = currentTime
+                })
+
+                if insertResult then
+                    TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 255, 255}, "You are now on duty as a " .. department)
+                else
+                    TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Failed to go on duty. Please try again.")
+                end
             else
-                TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Failed to go on duty. Please try again.")
+                TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You don't have permission to go on duty as a " .. department)
             end
         else
-            TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You don't have permission to go on duty as a " .. department)
+            TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Invalid department")
         end
-    else
-        TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "Invalid department")
     end
 end)
 
@@ -30,7 +39,7 @@ RegisterServerEvent("fd-duty:offDuty")
 AddEventHandler("fd-duty:offDuty", function(department)
     local source = source
     local discord = ExtractIdentifiers(source).discord:gsub("discord:", "")
-    local currentTime = os.time() / 60
+    local currentTime = os.time() / 60 
 
     local result = MySQL.Sync.fetchScalar("SELECT start_time FROM onduty WHERE discord = @discord AND department = @department ORDER BY start_time DESC LIMIT 1", {
         ['@discord'] = discord,
@@ -49,13 +58,13 @@ AddEventHandler("fd-duty:offDuty", function(department)
         })
 
         if insertResult then
-            local webhookUrl = Config.webhooks[department] 
+            local webhookUrl = Config.webhooks[department] -- Replace with your webhook URL
             local message = {
                 ["username"] = "Duty Log",
                 ["content"] = "<@" .. discord .. "> has gone off duty.",
                 ["embeds"] = {{
                     ["title"] = "Duty Log",
-                    ["description"] = "Department: " .. department .. "\nCallsign: " .. callsign .. "\nTime: " .. dutyTime .. " minutes",
+                    ["description"] = "Department: " .. department .. "\nTime: " .. dutyTime .. " minutes",
                     ["color"] = 16711680 -- Red color
                 }}
             }
